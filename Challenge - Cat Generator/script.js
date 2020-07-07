c65868e8e7c651d8f216246dbd828cb7a7fd211d
Ben Brownsell <ben.brownsell@hotmail.co.uk>
2020-07-07 13:16:07 +0100

    Add files via upload
    exploring the DOM with a simple cat generator.

---
+++ Challenge - Cat Generator/script.js
@@ -0,0 +1,24 @@
+// Challenge 1: Your age in days
+
+function ageInDays() {
+    var birthYear = prompt('What year were you born?');
+    var ageInDayss = (2020 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+// Challenge 2: Cat Generator
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://cdn2.thecatapi.com/images/4cl.gif";
+    div.appendChild(image);
+}
